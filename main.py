eedd69750a3500e7c8c78b44209235b764e6d793
Pranav Boyapati <pranav13b@gmail.com>
2025-06-18 22:01:19 -0500

    Debug deployment

--- main.py
+++ main.py
@@ -12,5 +12,5 @@ async def getActivityHistoryData(request: Request):
     sentData = await request.body()
     headers = {"Content-Type": "application/json"} 
     result = requests.post(url="https://script.google.com/macros/s/AKfycbxk6qa0p1dQjXrpodWTmKEeV92i8q0jGKMHeuS_QFnVpUAVi6X_5y_nnir4Y9I41OKw/exec", data=sentData, headers=headers)
-    return result.text
+    return result
 
