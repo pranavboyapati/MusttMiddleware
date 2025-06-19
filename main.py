5dd5566b88bfbb284458a02c14087fe0879a88e9
Pranav Boyapati <pranav13b@gmail.com>
2025-06-18 21:38:26 -0500

    Update test function

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import re
 import os
 from fastapi import FastAPI, HTTPException, Request
 from pydantic import BaseModel
+import requests
 
 app = FastAPI()
 
 @app.post("get-activity-history-data")
-def getActivityHistoryData(data):
-    return {"status": "success"}
+def getActivityHistoryData(sentData):
+    result = requests.post(url="https://script.google.com/macros/s/AKfycbxk6qa0p1dQjXrpodWTmKEeV92i8q0jGKMHeuS_QFnVpUAVi6X_5y_nnir4Y9I41OKw/exec", data=sentData)
+    return result
